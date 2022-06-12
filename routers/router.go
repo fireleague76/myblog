7e0cee644001216d917cbad7933573cfbb7692f7
Fire <157352407@qq.com>
2022-06-12 16:43:14 +0800

    main

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,18 @@ package routers
 
 import (
 	"myblog/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.BlogController{}, "*:Home")
+	beego.Router("/home", &controllers.BlogController{}, "*:Home")
+	beego.Router("/article", &controllers.BlogController{}, "*:Article")
+	beego.Router("/detail", &controllers.BlogController{}, "*:Detail")
+	beego.Router("/about", &controllers.BlogController{}, "*:About")
+	beego.Router("/timeline", &controllers.BlogController{}, "*:Timeline")
+	beego.Router("/resource", &controllers.BlogController{}, "*:Resource")
+	beego.Router("/comment", &controllers.BlogController{}, "post:=Comment")
+	beego.AutoRouter(&controllers.AdminController{})
 }
